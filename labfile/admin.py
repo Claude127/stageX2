c13c072669aaa399d3c7e41a2655166c38d0c33d
Claude127 <claude.djiojip@2026.ucac-icam.com>
2023-06-08 13:32:20 +0100

    set on authentification mecanisme

--- labfile/admin.py
+++ labfile/admin.py
@@ -1,13 +1,43 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.forms import Textarea
 
 from .models import Utilisateur, Role, Action, Document
 
-# Register your models here.
 
-admin.site.register(Utilisateur)
-admin.site.register(Role)
-admin.site.register(Action)
-admin.site.register(Document)
+class UserAdminConfig(UserAdmin):
+    search_fields = ('email', 'role', 'nom', 'prenom',)
+    list_filter = ('email', 'role', 'is_active', 'is_staff')
+    ordering = ('date_creation',)
+    list_display = ('email', 'nom', 'prenom', 'role', 'is_active', 'is_staff')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'nom', 'prenom', 'image', 'role',)}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'nom', 'prenom', 'image', 'role', 'password1', 'password2', 'is_active', 'is_staff')
+        }
+
+         )
+    )
+
+
+class DocumentAdmin(admin.ModelAdmin):
+    list_display =  ('nom', 'categorie')
+    search_fields = ('nom', 'categorie')
+    ordering = ('nom', 'categorie')
+    list_filter = ('nom', 'categorie')
+
 
 # # custom
 # admin.site.index_template = 'admin/dashboard.html',
+# Register your models here.
+admin.site.register(Utilisateur, UserAdminConfig)
+admin.site.register(Role)
+admin.site.register(Action)
+admin.site.register(Document, DocumentAdmin)
--- labfile/views.py
+++ labfile/views.py
@@ -1,116 +1,44 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-import pymysql
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
-
-rm = ''
-em = ''
-pwd = ''
-
-from .models import Utilisateur
 
 
 # Create your views here.
 
 # gestion de connexion et deconnexion de comptes
 def login_user(request):
-    # global em, pwd,rm
-    # if request.method == 'POST':
-    #
-    #     # etablir la connexion a la base de donnees
-    #     conn = pymysql.connect(
-    #         host='localhost',
-    #         user='root',
-    #         password='',
-    #         db='labfile',
-    #         port=3306
-    #     )
-    #     # recupere les informations du formulaire
-    #     d = request.POST
-    #     for key, value in d.items():
-    #         if key == "email":
-    #             em = value
-    #         if key == "password":
-    #             pwd = value
-    #         if key == "remember":
-    #             rm =value
-    #     # remember me
-    #     if rm:
-    #         # cas:case "se souvenir de moi" est cochee, definir un cookie
-    #         max_age = 14*24*60*60 #la duree du cookie est de 14jours
-    #         response = redirect('file')
-    #         response.set_cookie('em', em, max_age=max_age)
-    #         response.set_cookie('pwd', pwd, max_age=max_age)
-    #     else:
-    #         # cas: case "se souvenir de moi" est decochee ,supprimer les cookies
-    #         response = redirect('file')
-    #         response.delete_cookie('em')
-    #         response.delete_cookie('pwd')
-    #
-    #     #executer la commande pour authentifier l'utilisateur
-    #     c = "select * from labfile_utilisateur where email='{}' and password='{}' ".format(em, pwd)
-    #     # curseur pour executer la commande
-    #     cursor = conn.cursor()
-    #     cursor.execute(c)
-    #     t = tuple(cursor.fetchall())
-    #     if t == ():
-    #         messages.success(request, "une erreur est survenue , veuillez reesayer...")
-    #         return redirect('login')
-    #     else:
-    #         request.session['email'] = em
-    #         return response
-    #         # verif session ouverte
-    #         # message = f"Bienvenue,{em}!"
-    #         # return HttpResponse(message)
-    # else:
-    #     #l'utilisateur est deja connecté
-    #     if 'em' in request.session:
-    #         return redirect('file')
-    #     #verifier si les cookie sont presents
-    #     em = request.COOKIES.get('em')
-    #     pwd = request.COOKIES.get('pwd')
-    #
-    #     #si les cookies sont operationels ,se connecter automatiquement
-    #
-    #     if em and pwd:
-    #         # etablir la connexion a la base de donnees
-    #         conn = pymysql.connect(
-    #             host='localhost',
-    #             user='root',
-    #             password='',
-    #             db='labfile',
-    #             port=3306
-    #         )
-    #         # executer la commande pour authentifier l'utilisateur
-    #         c = "select * from labfile_utilisateur where email='{}' and password='{}' ".format(em, pwd)
-    #         # curseur pour executer la commande
-    #         cursor = conn.cursor()
-    #         cursor.execute(c)
-    #         t = tuple(cursor.fetchall())
-    #         if t == ():
-    #             messages.success(request, "une erreur est survenue , veuillez reesayer...")
-    #             return redirect('login')
-    #         else:
-    #             request.session['email'] = em
-    #             return redirect('file')
-    #             # verif session ouverte
-    #             # message = f"Bienvenue,{em}!"
-    #             # return HttpResponse(message)
-        #afficher la page de connexion
-        return render(request, 'login.html')
+    # recupère l'email de l'utilisateur a partir du cookie , s'il est present
+    email = request.COOKIES.get('email', '')
+
+    # traitement du formulaires
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        remember = request.POST.get('remember')
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            response = redirect('file')
+
+            if remember:
+                response.set_cookie('email', email, max_age=14 * 24 * 60 * 60)  # cookie valable pendant 2 semaines
+                return response
+        else:
+            messages.error(request, 'valeurs incorrectes, veuillez réessayer...')
+            return redirect('login')
+    else:
+        # afficher la page de connexion avec l'email prerempli s'il existe
+        return render(request, 'login.html',{'email': email})
 
 
 def logout_user(request):
-    #supprimer la session de l'utilisateur actuel
-    request.session.flush()
+    logout(request)
 
-    #supprimer les cookies pour l'email er le mot de passe
-    response =redirect('login')
-    response.delete_cookie('em')
-    response.delete_cookie('pwd')
-
-    return response
+    response = redirect('login')
+    response.delete_cookie('email')
+    messages.success(request, 'you were logged out :)')
+    return response('login')
 
 
 def file(request):
