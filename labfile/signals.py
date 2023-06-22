c1854f3a78d5c54820cd93999eff7b2565ad4641
Claude127 <claude.djiojip@2026.ucac-icam.com>
2023-06-22 09:18:28 +0100

    alter send-mail

--- labfile/signals.py
+++ labfile/signals.py
@@ -9,8 +9,12 @@ from django.conf import settings
 @receiver(post_save, sender=Utilisateur)
 def send_email_on_create(sender, instance, created, **extra_fields):
     if created:
-        subject = 'Bienvenue sur notre site'
-        message = f'Bonjour {instance.prenom},\n\nBienvenue sur notre site !\n\nCordialement,\nL\'équipe de notre site'
+        subject = 'Bienvenue à LAB2VIEW'
+        message = f'Bienvenue {instance.prenom},\n\n L\'équipe de LAB2VIEW est heureuse de vous compter ' \
+                  f'parmi ses effectifs :) \nCi-joint vos identifiants pour l\'accès à Labfile : Notre application de ' \
+                  f'stockage de documents \n\n Lien de connexion : localhost:8000 \n Email :{instance.email} \n Mot ' \
+                  f'de passe (par défaut) : labf!le123 \n\nNB: le mot de passe doit etre modifié après la prémière ' \
+                  f'connexion!   \n\nCordialement,\nL\'équipe de LAB2VIEW'
         email_from = settings.EMAIL_HOST_USER
         recipient_list = [instance.email, ]
         send_mail(subject, message, email_from, recipient_list)
